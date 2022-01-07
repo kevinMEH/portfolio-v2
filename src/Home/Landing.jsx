0ae5e9ca4ed4dbbcfe8867d8664de4361d9371dc
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2022-01-07 12:43:27 -0500

    Landing laptop support

--- src/Home/Landing.jsx
+++ src/Home/Landing.jsx
@@ -2,10 +2,11 @@ const Landing = () => {
     return (
         <div className="w-full landing">
             <div className="landing2 landing2 flex h-screen">
-                <div className="text-center my-auto px-8 container mx-auto">
-                    <h1 className="text-gray-200 font-medium tracking-wide text-lg">Hello! 👋</h1>
-                    <p className="text-white font-bold text-4xl gradientText">I'm Kevin,</p>
-                    <p className="text-gray-300 pt-1">a web developer based in Brooklyn. I create unique and memorable web experiences.</p>
+                <div className="text-center my-auto px-8 container mx-auto lg:max-w-screen-md lg:space-y-3">
+                    <h1 className="text-gray-200 font-medium tracking-wide text-lg lg:text-xl">Hello! 👋</h1>
+                    <p className="text-white font-bold text-4xl gradientText lg:text-6xl">I'm Kevin,</p>
+                    <p className="text-gray-300 pt-1 lg:text-xl lg:px-36 ">a web developer based in Brooklyn. I design
+                     unique and memorable web experiences.</p>
                 </div>
             </div>
         </div>
