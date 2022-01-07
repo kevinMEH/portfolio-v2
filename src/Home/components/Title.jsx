e5a14f063e4a338c29ea6db0f394010a0a3f7990
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2022-01-06 19:20:02 -0500

    Title changes

--- src/Home/components/Title.jsx
+++ src/Home/components/Title.jsx
@@ -1,6 +1,6 @@
 const Title = ({label, text}) => {
     return (
-        <div className="mb-4 space-y-2">
+        <div className="mb-3 space-y-2">
             <h1 className="text-gray-600 font-black tracking-wide uppercase text-sm">{"— " + label}</h1>
             <h1 className="text-gray-300 font-semibold tracking-wide text-2xl">{text}</h1>
         </div>
