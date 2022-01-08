946cd398ff5d58cd055071afe10c46496f81e86a
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2022-01-07 20:35:04 -0500

    Support for all screen sizes

--- src/Home/components/Title.jsx
+++ src/Home/components/Title.jsx
@@ -1,8 +1,8 @@
 const Title = ({label, text, center = false}) => {
     return (
-        <div className={center ? "text-center mb-3 space-y-2" : "mb-3 space-y-2"}>
-            <h1 className="text-gray-600 font-black tracking-wide uppercase text-sm md:text-base lg:text-lg">{center ? label : "— " + label}</h1>
-            <h1 className="text-gray-300 font-semibold tracking-wide text-2xl md:text-3xl xl:text-4xl">{text}</h1>
+        <div className={center ? "text-center mb-3 space-y-2 xl:mb-5 xl:space-y-3" : "mb-3 space-y-2 xl:mb-5 xl:space-y-3"}>
+            <h1 className="text-gray-600 font-black tracking-wide uppercase text-sm md:text-base lg:text-lg 2xl:text-xl">{center ? label : "— " + label}</h1>
+            <h1 className="text-gray-300 font-semibold tracking-wide text-2xl md:text-3xl xl:text-4xl 2xl:text-5xl">{text}</h1>
         </div>
     )
 }
